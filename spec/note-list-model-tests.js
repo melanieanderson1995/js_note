280338a61db9463f27904beaf8824bc1555bf133
Melanie Anderson <melanieanderson1995@gmail.com>
2020-08-17 16:43:46 +0100

    finished note list model and tests

--- spec/note-list-model-tests.js
+++ spec/note-list-model-tests.js
@@ -1,13 +1,20 @@
 function testThatNewNoteListIsCreated() {
   var notelist = new Notelist();
-  assert.isTrue(notelist.notes.length == 0);
+  assert.isTrue(notelist.notes.length === 0);
 }
 testThatNewNoteListIsCreated();
 
 function addNewNoteToArray() {
   var notelist = new Notelist();
   notelist.create("hello");
-  assert.isTrue(notelist.notes.length == 1);
+  assert.isTrue(notelist.notes.length === 1);
 }
-
 addNewNoteToArray();
+
+function returnsNotesInArray() {
+  var notelist = new Notelist();
+  notelist.create("hello");
+  notelist.create("goodbye");
+  assert.isTrue(notelist.all().length === 2);
+}
+returnsNotesInArray();
--- src/note-list-model.js
+++ src/note-list-model.js
@@ -9,5 +9,9 @@ Notelist.prototype.create = function (message) {
   this.notes.push(message);
 }
 
+Notelist.prototype.all = function () {
+  return this.notes;
+}
+
   exports.Notelist = Notelist;
 })(this);
